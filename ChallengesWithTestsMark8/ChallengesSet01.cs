7a0c865c80e2123f3c16d2759f50cb9f4d6c3d86
Kalkidan Shibeshi <kkalitemesgen@gmail.com>
2023-02-24 15:17:44 -0500

    set 1 commit

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -18,32 +18,54 @@ namespace ChallengesWithTestsMark8
 
         public double Subtract(double minuend, double subtrahend)
         {
+            return minuend- subtrahend;
             throw new NotImplementedException();
         }
 
         public int Add(int number1, int number2)
         {
+            return number1 + number2;
             throw new NotImplementedException();
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
+
+            if(number1 < number2)
+            {
+                return number1;
+            }
+            else
+            {
+                return number2;
+            }
             throw new NotImplementedException();
         }
 
         public long Multiply(long factor1, long factor2)
         {
+            return factor1 * factor2;
             throw new NotImplementedException();
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+            if (nameOfPerson == "")
+            {
+                return "Hello!";
+            }
+            else
+            {
+                return $"Hello, {nameOfPerson}!";
+            }
         }
 
         public string GetHey()
+
         {
-            throw new NotImplementedException();
+            return "HEY!";
+            
+            
         }
     }
 }
